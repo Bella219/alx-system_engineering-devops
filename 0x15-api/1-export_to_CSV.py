1b595cd469adda9d8f6eed940bf71b79bb81f9af
Bella219 <cho219bella@yahoo.com>
2023-04-24 21:27:21 +0000

    Task 1 Python Script extended to export data in the CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 """
 Python script that that returns information about a
-given employee ID TODO list progress,
-exports data in the CSV format.
+given employee ID TODO list progress
+extended to export data in the CSV format
 """
 import csv
 import requests
 import sys
 
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-if __name__ == '__main__':
-    id_c = sys.argv[1]
-    url_user = "https://jsonplaceholder.typicode.com/users/" + id_c
-    res = requests.get(url_user).json()
-    username = res.get("username")
-    req = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' +
-        (id_c) + '/todos')
-    with open("{}.csv".format(sys.argv[1]), "w") as file_c:
-        writer = csv.writer(file_c, quoting=csv.QUOTE_ALL)
-        for task in req.json():
-            writer.writerow([id_c, username,
-                            task.get("completed"), task.get("title")])
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]

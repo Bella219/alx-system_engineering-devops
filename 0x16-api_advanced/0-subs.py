095532cc87504019aa9cc7020d437dc8c742c99d
Bella219 <cho219bella@yahoo.com>
2023-05-09 20:19:37 +0000

    A function that queries the Reddit API and returns the number of subscribers for a given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A function that queries the Reddit API and returns the number of subscribers"""
+
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    user = {"User-Agent": "Bella"}
+    request = requests.get("https://www.reddit.com/r/{}/about.json"
+                           .format(subreddit), headers=user)
+    if request.status_code == 200:
+        return request.json().get("data").get("subscribers")
+    else:
+        return 0
